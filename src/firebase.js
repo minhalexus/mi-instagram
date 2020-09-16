245df7d15c2b9fa6ba5748682ddb6333e28b3306
Minhal Shanjer <mshanjer@gmail.com>
2020-09-15 22:15:14 -0400

    Fixed firebase bug

--- src/firebase.js
+++ src/firebase.js
@@ -11,7 +11,7 @@ import firebase from "firebase";
 //     measurementId: "G-DTJ3XYL88Q"
 // });
 
-var firebaseConfig = {
+var firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyD3GuHVvmkYOHTftZsBYPfhFo-Ndx4S0oY",
     authDomain: "instagram-clone-82ba3.firebaseapp.com",
     databaseURL: "https://instagram-clone-82ba3.firebaseio.com",
@@ -20,7 +20,7 @@ var firebaseConfig = {
     messagingSenderId: "598505826222",
     appId: "1:598505826222:web:d6c07765ced954cb300a3f",
     measurementId: "G-DE691S023K"
-  };
+  });
 
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
